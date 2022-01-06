8d4963232ec35815463bc2f3132d38331790441e
Micah Wood <micah@wpscholar.com>
2022-01-06 09:59:11 -0500

    Update wp-forge/helpers to 2.0

---
+++ includes/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+use function \WP_Forge\Helpers\dataGet;
+use function \WP_Forge\Helpers\dataSet;
+
+function data_get( $data, $key, $default = null ) {
+	return dataGet( $data, $key, $default = null );
+}
+
+function data_set( &$target, $key, $value, $overwrite = true ) {
+	return dataSet( $target, $key, $value, $overwrite = true );
+}
