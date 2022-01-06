eb6242dfa58cd9c25e3e0151e985dd31201ae934
Micah Wood <micah@wpscholar.com>
2022-01-06 10:03:21 -0500

    Update function signature

--- includes/functions.php
+++ includes/functions.php
@@ -4,9 +4,9 @@ use function \WP_Forge\Helpers\dataGet;
 use function \WP_Forge\Helpers\dataSet;
 
 function data_get( $data, $key, $default = null ) {
-	return dataGet( $data, $key, $default = null );
+	return dataGet( $data, $key, $default );
 }
 
 function data_set( &$target, $key, $value, $overwrite = true ) {
-	return dataSet( $target, $key, $value, $overwrite = true );
+	return dataSet( $target, $key, $value, $overwrite );
 }
